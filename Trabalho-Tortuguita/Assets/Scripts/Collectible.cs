61ce81f878790c5f6ad659d177810506c5c2dc7c
Maria Eduarda Biscalquini Silva <00126949@unisoacad.uniso.br>
2026-03-30 22:11:46 -0300

    teste cenario

---
+++ Trabalho-Tortuguita/Assets/Scripts/Collectible.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Collectible : MonoBehaviour
+{
+    public int points = 1;
+
+    public float rotationSpeed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateObject();
+    }
+    
+    void RotateObject()
+    {
+        transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Trabalho-Tortuguita/Assets/Scripts/PlayerColector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColector : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Collectible"))
+        {
+            Collectible collectible = other.GetComponent<Collectible>(); 
+            
+            if(collectible != null)
+            {
+                gameManager.AddPoints(collectible.points);
+            }
+
+            Destroy(other.gameObject);
+        }
+    }
+
+    
+}
